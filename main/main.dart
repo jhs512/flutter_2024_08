2b3a1b1c534ce58ff72c6589fa8d6de53b725f23
jhs512 <jangka512@gmail.com>
2024-08-16 00:07:44 +0900

    work

--- main/main.dart
+++ main/main.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 void main() {
   stdout.write('명령) ');
 
-  String? command = stdin.readLineSync();
+  String command = stdin.readLineSync()!;
 
-  print("command.length: ${command?.length}");
+  print("command.length: ${command.length}");
   print("command: $command");
 }
